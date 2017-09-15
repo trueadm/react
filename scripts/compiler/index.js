8d8d826f9cc281264920794a7f125eb53a86ddcf
Dan Abramov <dan.abramov@gmail.com>
2017-09-15 21:02:13 +0100

    Add fixture demonstrating a bug

--- scripts/compiler/index.js
+++ scripts/compiler/index.js
@@ -2,9 +2,13 @@
 
 const setupSource = require('./src/setup').setupSource;
 const compileSource = require('./src/compiler').compileSource;
+const getOptimizedTrees = require('./src/optimizer').getOptimizedTrees;
 
 async function compileSourceEntry(source) {
-	return setupSource(source).then(compileSource);
+	return setupSource(source).then(compileSource).then(code => ({
+    code,
+    optimizedTrees: getOptimizedTrees(),
+  }));
 }
 
 module.exports = compileSourceEntry;
