899c96f0f5205265a8b94512870c275a2b4d351c
Dominic Gannaway <dg@domgan.com>
2017-10-04 19:30:14 +0100

    WIP

--- scripts/compiler/examples/tests/test.js
+++ scripts/compiler/examples/tests/test.js
@@ -8,57 +8,14 @@
  */
 'use strict';
 
-var React = require('react');
-
-class C extends React.Component {
-  constructor() {
-    super();
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick(e) {
-    // do something
-  }
-  render() {
-    return <div onClick={this.handleClick}>!</div>;
-  }
-}
-
-class D extends React.Component {
-  constructor() {
-    super();
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick(e) {
-    // do something
-  }
-  render() {
-    return <div onClick={this.handleClick}>!</div>;
-  }
-}
-
-
-function A(props: {x: number}) {
-  return <div>Hello {props.x}</div>;
-}
-
-function B() {
-  return <div>World</div>;
-}
-
-class Lol {
-  foo() {
-
-  }
-}
+const ExternalModule = require('ExternalModule');
 
 function App() {
-  var lol = new Lol();
+  var lol = ExternalModule.foo();
   return (
     <div>
-      <A x={42} />
-      <B />
-      <C />
-      <D />
+      {lol}
+
     </div>
   );
 }
