d25d3bb31f0e765dfc06811e3387eaa52d928f1b
Dan <dan.abramov@gmail.com>
2018-05-13 02:13:43 +0100

    Split ReactNoop into normal and persistent exports

--- packages/react-reconciler/src/__tests__/ReactPersistent-test.internal.js
+++ packages/react-reconciler/src/__tests__/ReactPersistent-test.internal.js
@@ -29,7 +29,7 @@ xdescribe('ReactPersistent', () => {
   });
 
   function render(element) {
-    ReactNoopPersistent.renderToPersistentRootWithID(element, DEFAULT_ROOT_ID);
+    ReactNoopPersistent.render(element);
   }
 
   function div(...children) {
@@ -42,7 +42,7 @@ xdescribe('ReactPersistent', () => {
   }
 
   function getChildren() {
-    return ReactNoopPersistent.getChildren(DEFAULT_ROOT_ID);
+    return ReactNoopPersistent.getChildren();
   }
 
   it('can update child nodes of a host instance', () => {
