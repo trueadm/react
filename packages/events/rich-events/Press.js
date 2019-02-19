8764095f08fa3dbc4afa6a632186ff4101a60525
Dominic Gannaway <dg@domgan.com>
2019-02-19 18:16:50 +0000

    WIP

--- packages/events/rich-events/Press.js
+++ packages/events/rich-events/Press.js
@@ -9,43 +9,68 @@
 
 const listenTo = [
   'onKeyPress',
-  'onClick',
   'onPointerDown',
-  'onPointerUp',
-  'onPointerCancel',
 ];
 
 // In the case we don't have PointerEvents (Safari), we listen to touch events
 // too
 if (typeof window !== 'undefined' && window.PointerEvent === undefined) {
-  listenTo.push('onTouchStart', 'onTouchEnd', 'onTouchCancel', 'onMouseDown', 'onMouseUp');
+  listenTo.push('onTouchStart', 'onMouseDown');
 }
 
-const PressImpl = {
+function handlePressOut(state, context, e) {
+  if (!state.isPressed) {
+    return;
+  }
+  if (state.onPressChange !== null) {
+    state.onPressChange();
+  }
+  if (state.onPressOut !== null) {
+    state.onPressOut(e);
+  }
+  state.isPressed = false;
+  if (e.type === "pointercancel" || e.type === "touchcancel") {
+    state.pressStartFiber = null;
+    return;
+  }
+  if (state.pressStartFiber !== null && state.onPress !== null) {
+    const target= e.target;
+    let traverseFiber = context.getClosestInstanceFromNode(target);
+    let triggerPress = false;
+
+    while (traverseFiber !== null) {
+      if (traverseFiber === state.pressStartFiber) {
+        triggerPress = true;
+      }
+      traverseFiber = traverseFiber.return;
+    }
+    if (triggerPress) {
+      state.onPress(e);
+    }
+  }
+  state.pressStartFiber = null;
+}
+
+const PressImplementation = {
   listenTo,
   createInitialState(config) {
-    return {
+    const state = {
       isPressed: false,
-      simulateClick: false,
-      touchFiber: null,
+      onPress: null,
+      onPressChange: null,
+      onPressOut: null,
+      pressStartFiber: null,
     };
+    return state;
   },
   processRichEvents(
     context,
-    config,
+    props,
     state,
   ): void {
     const { eventTarget, eventTargetFiber, eventType, eventListener, nativeEvent, richEventType } = context;
 
-    if (eventType === 'click' || eventType === 'keypress') {
-      if (state.simulateClick) {
-        state.simulateClick = false;
-        return;
-      }
-      if (richEventType === 'onPress') {
-        let richEventListener = eventListener;
-
-        if (eventType === 'keypress') {
+    if (eventType === 'keypress') {
           const isValidKeyPress =
             nativeEvent.which === 13 ||
             nativeEvent.which === 32 ||
@@ -55,31 +80,25 @@ const PressImpl = {
             return;
           }
           // Wrap listener with prevent default behaviour
-          richEventListener = e => {
+          const keyPressEventListener = e => {
             if (!e.isDefaultPrevented() && !e.nativeEvent.defaultPrevented) {
               e.preventDefault();
               eventListener(e);
             }
           };
-        }
         context.dispatchTwoPhaseEvent(
           'press',
-          richEventListener,
+          keyPressEventListener,
           nativeEvent,
           eventTarget,
           eventTargetFiber,
           false,
         );
-      }
     } else if (
       eventType === 'pointerdown' ||
       eventType === 'touchstart' ||
       eventType === 'mousedown'
     ) {
-      if (eventType === 'touchstart' || eventType === 'pointerdown') {
-        state.simulateClick = true;
-        state.touchFiber = eventTargetFiber;
-      }
       if (richEventType === 'onPressIn') {
         context.dispatchTwoPhaseEvent(
           'pressin',
@@ -93,100 +112,48 @@ const PressImpl = {
         if (!state.isPressed) {
           eventListener(true);
         }
-        state.isPressed = true;
       }
-    } else if (
-      eventType === 'pointerup' ||
-      eventType === 'pointercancel' ||
-      eventType === 'touchend' ||
-      eventType === 'touchcancel' ||
-      eventType === 'mouseup'
-    ) {
-      if (richEventType === 'onPressOut') {
-        context.dispatchTwoPhaseEvent(
-          'pressup',
-          eventListener,
-          nativeEvent,
-          eventTarget,
-          eventTargetFiber,
-          false,
-        );
-      } else if (richEventType === 'onPressChange') {
+      if (!state.isPressed) {
+        state.isPressed = true;
+        state.pressStartFiber = eventTargetFiber;
+        const pressUpEventListener = e => {
+          document.removeEventListener('mouseup', pressUpEventListener);
+          document.removeEventListener('pointerup', pressUpEventListener);
+          document.removeEventListener('touchend', pressUpEventListener);
+          document.removeEventListener('touchcancel', pressUpEventListener);
+          handlePressOut(state, context, e);
+        }
+        document.addEventListener('mouseup', pressUpEventListener);
+        document.addEventListener('pointerup', pressUpEventListener);
+        document.addEventListener('touchend', pressUpEventListener);
+        document.addEventListener('touchcancel', pressUpEventListener);
+      }
+    } else if (richEventType === 'onPressChange') {
+      state.onPressChange = e => {
         if (state.isPressed) {
           eventListener(false);
         }
-        state.isPressed = false;
-      }
-      if (state.simulateClick && state.touchFiber !== null) {
-        let traverseFiber = eventTargetFiber;
-        let triggerPress = false;
-
-        while (traverseFiber !== null) {
-          if (traverseFiber === eventTargetFiber) {
-            triggerPress = true;
-          }
-          traverseFiber = traverseFiber.return;
+        state.onPressChange = null;
+      };
+    } else if (richEventType === 'onPressOut') {
+      state.onPressOut = e => {
+        if (state.isPressed) {
+          eventListener(e);
         }
-        if (triggerPress) {
-          context.dispatchTwoPhaseEvent(
-            'press',
-            eventListener,
-            nativeEvent,
-            eventTarget,
-            eventTargetFiber,
-            false,
-          );
-        }
-        state.touchFiber = null;
-      }
+        state.onPressOut = null;
+      };
+    } else if (richEventType === 'onPress') {
+      state.onPress = e => {
+        eventListener(e);
+        state.onPress = null;
+      };
     }
   },
 };
 
-export function onPress(config) {
+export default function press(props) {
   return {
-    type: 'onPress',
-    config,
-    impl: PressImpl,
+    impl: PressImplementation,
+    props,
   };
 }
-
-onPress.type = 'onPress';
-onPress.config = null;
-onPress.impl = PressImpl;
-
-export function onPressIn(config) {
-  return {
-    type: 'onPressIn',
-    config,
-    impl: PressImpl,
-  };
-}
-
-onPressIn.type = 'onPressIn';
-onPressIn.config = null;
-onPressIn.impl = PressImpl;
-
-export function onPressOut(config) {
-  return {
-    type: 'onPressOut',
-    config,
-    impl: PressImpl,
-  };
-}
-
-onPressOut.type = 'onPressOut';
-onPressOut.config = null;
-onPressOut.impl = PressImpl;
-
-export function onPressChange(config) {
-  return {
-    type: 'onPressChange',
-    config,
-    impl: PressImpl,
-  };
-}
-
-onPressChange.type = 'onPressChange';
-onPressChange.config = null;
-onPressChange.impl = PressImpl;
