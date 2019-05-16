04cddee71e60a0e6ac8ac99fc89a3d779afa9708
Dominic Gannaway <dg@domgan.com>
2019-05-16 23:28:43 +0100

    Optimize and clean up setTextContent

--- packages/react-dom/src/client/setTextContent.js
+++ packages/react-dom/src/client/setTextContent.js
@@ -7,8 +7,6 @@
  * @flow
  */
 
-import {TEXT_NODE} from '../shared/HTMLNodeType';
-
 /**
  * Set the textContent property of a node. For text updates, it's faster
  * to set the `nodeValue` of the Text node directly instead of using
@@ -22,11 +20,7 @@ let setTextContent = function(node: Element, text: string): void {
   if (text) {
     let firstChild = node.firstChild;
 
-    if (
-      firstChild &&
-      firstChild === node.lastChild &&
-      firstChild.nodeType === TEXT_NODE
-    ) {
+    if (firstChild != null) {
       firstChild.nodeValue = text;
       return;
     }
